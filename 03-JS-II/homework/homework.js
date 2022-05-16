669333dbc0867923e776941ecc0eef6773875bc3
jose lopez fandino <joselofan@hotmail.com>
2022-05-16 11:07:31 -0500

    Finish HomeWork II

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -96,7 +96,7 @@ function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
-  if(numero==10 || numero==5){
+  if(numero==5 || numero==10){
     return true;
   } else {
     return false;
